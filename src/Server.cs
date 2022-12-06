0a3c4814e82aa05f1749e1ee21a78a5382de012b
Александр <akhabirov@rallyup.com>
2022-12-06 11:36:57 +0500

    send response

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,4 +8,7 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+const string response = "+OK\\r\\n";
+var bytes = Encoding.ASCII.GetBytes(response);
+socket.Send(bytes);
